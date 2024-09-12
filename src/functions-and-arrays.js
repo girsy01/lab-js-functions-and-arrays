c89cf6bb021f56df2bbd0581212e9fdc64d72a37
Franziska Scheer <franzi-scheer@web.de>
2024-09-12 19:17:58 +0200

    lab done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,78 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) return num1;
+  else return num2;
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  if (!arr.length) return null;
 
-
-
+  index = 0;
+  longest = 0;
+  arr.forEach((element, i) => {
+    if (element.length > longest) {
+      index = i;
+      longest = element.length;
+    }
+  });
+  return arr[index];
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(arr) {
+  sum = 0;
+  arr.forEach((e) => (sum += e));
+  return sum;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(arr) {
+  if (!arr.length) return 0;
+  sum = sumNumbers(arr);
+  return sum / arr.length;
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+  console.log(word, arr);
+  if (!arr.length) return null;
+//   arr.forEach((e) => {
+//     console.log("Check: ", e, word, e === word);
+//     if (e === word) {
+//       console.log("TRUE");
+//       return true;
+//     }
+//   });
+    
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === word) return true;
+    }
+    return false;
+  
+}
